50d0ad9894dd1911ca888ea0a7a2b2b7bc69b642
KasunUmesh <kasunumesh38@gmail.com>
2023-09-15 23:44:31 +0530

    Add Project Dependencies and application Properties file create

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,15 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.springframework:spring-webmvc:5.3.22")
     compileOnly("javax.servlet:javax.servlet-api:4.0.1")
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    implementation("org.hibernate:hibernate-core:5.6.9.Final")
+    implementation("mysql:mysql-connector-java:8.0.29")
+    implementation("org.springframework.data:spring-data-jpa:2.5.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("org.springframework:spring-orm:5.3.22")
+    implementation("org.springframework:spring-context:5.3.21")
+    implementation("org.modelmapper:modelmapper:3.1.1")
+
 
 
 }
